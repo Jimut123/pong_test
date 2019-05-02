8dd0e748b5338b5a775ec7afe10c05da3c579be5
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-05-02 23:11:57 +0530

    done sigmoid

---
+++ cnn_implementation/COLAB/testing.py
@@ -0,0 +1,49 @@
+from keras.models import load_model
+import numpy as np
+import cv2
+import time
+import pyscreenshot as ImageGrab
+import pyautogui
+import time
+# only up and down, no standing!
+#loading the model
+model = load_model('Pong_Thu May  2 16_50_13 2019.h5')
+    
+def main():
+
+    for i in list(range(5))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
+    while(True):
+        
+            # 640x480 windowed mode
+            screen=ImageGrab.grab(bbox=(390,290,990,695))
+            screen = cv2.cvtColor(np.float32(screen), cv2.COLOR_BGR2GRAY)
+            screen = cv2.resize(screen,(64,64))
+            #cv2.imshow('screen',screen)
+            #cv2.waitKey(0)
+            screen = screen[np.newaxis,:,:,np.newaxis]
+            screen = np.array(screen)
+            
+            # model prediction
+            y_prob = model.predict(screen)
+            prediction = y_prob.argmax(axis=-1)
+            
+            
+            if prediction == 0:
+               pyautogui.keyUp('down')
+               start = time.time()
+               while time.time() - start < 0.7:
+                    pyautogui.keyDown('up')
+                    print('Up')
+            elif prediction == 1:
+               pyautogui.keyUp('up')
+               start = time.time()
+               while time.time() - start < 0.7:
+                    pyautogui.keyDown('down')
+                    print('Down')
+                 
+
+        
+main()       
--- cnn_implementation/COLAB/training.py
+++ cnn_implementation/COLAB/training.py
@@ -16,14 +16,14 @@ with open('actions.csv', 'w') as csv:
             os.mkdir(r'./images')
 
 
-        for i in list(range(8))[::-1]:
+        for i in list(range(3))[::-1]:
             print(i+1)
             time.sleep(1)
 
     
         while(True):
 
-            # 640x480 windowed mode [PLEASE CHANGE THIS ACCORDING TO YOUR SCREEN!]
+            # 640x480 windowed mode
             screen=ImageGrab.grab(bbox=(390,290,990,695))
             screen = cv2.cvtColor(np.float32(screen), cv2.COLOR_BGR2GRAY)
             #cv2.imshow('screen',screen)
@@ -34,27 +34,27 @@ with open('actions.csv', 'w') as csv:
             if keyboard.is_pressed('up arrow'):
                 if c < 1000:#1000 denotes the no of images for up action
                     cv2.imwrite('./images/frame_{0}.jpg'.format(x), screen)
-                    csv.write('1\n')
+                    csv.write('0\n')
                     print('up')
                     x += 1
                     c += 1
             elif keyboard.is_pressed('down arrow'):
                 if d < 1000:#1000 denotes the no of images for down action
                     cv2.imwrite('./images/frame_{0}.jpg'.format(x), screen)
-                    csv.write('2\n')
+                    csv.write('1\n')
                     print('down')
                     x += 1
                     d += 1
-            else:
-                if t < 1000:#1000 denotes the no of images for nothing action
-                    cv2.imwrite('./images/frame_{0}.jpg'.format(x), screen)
-                    csv.write('0\n')
-                    print('nothing')
-                    x += 1
-                    t += 1
+            # else:
+            #     if t < 10000:#10000 denotes the no of images for nothing action
+            #         cv2.imwrite('./images/frame_{0}.jpg'.format(x), screen)
+            #         csv.write('0\n')
+            #         print('nothing')
+            #         x += 1
+            #         t += 1
 
-            print("C = ",c,"/1000","T = ",t,"/1000","D = ",d,"/1000")
-            if c == 1000 and t == 1000 and d==1000:
+            print("C [UP] = ",c,"/10000","D [DOWN] = ",d,"/10000")
+            if c == 1000  and d==1000:
                 csv.close()
                 cv2.destroyAllWindows()
                 break                
